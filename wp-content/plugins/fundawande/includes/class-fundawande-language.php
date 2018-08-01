e414279608d2e146e11fbfbf35b04e5f6647461d
Chris <chris@pango.world>
2018-08-01 12:25:46 +0200

    Add first public funciton to FW language class with pseudocode

--- wp-content/plugins/fundawande/includes/class-fundawande-language.php
+++ wp-content/plugins/fundawande/includes/class-fundawande-language.php
@@ -23,5 +23,30 @@ class FundaWande_Language {
     public function __construct() {
 
     }
+    /**
+     *  Add a notice to the array of pastpapers for display at a later stage.
+     *
+     * @param array $language. This is the language variable as set by the url on each page via url parameters
+     *
+     * @return object $language_obj
+     */
+    public function get_language( $language = null ) {
+
+        // Determine if $language was assigned, if not then assign to the default language
+
+        // Create empty $language_obj
+
+        // Create switch case depending on the $language
+
+        // Add language prefix to $language_obj
+
+        // Add language url suffix to $language_obj
+
+        // Add language identifier to $language_obj
+
+        // return $language_obj
+
+
+    } // end get_language()
 
 } // end FundaWande_Language
