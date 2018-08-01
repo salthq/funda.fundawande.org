27f70230f518d99e49012427e8db05877f2d33e2
Chris <chris@pango.world>
2018-08-01 12:13:37 +0200

    Set up FW language class and rename to fundawande-language.php for longevity

---
+++ wp-content/plugins/fundawande/includes/class-fundawande-language.php
@@ -0,0 +1,27 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // security check, don't load file outside WP
+}
+
+
+/**
+ * FundaWande language Class
+ *
+ * All functionality pertaining to the language functionality of Funda Wande.
+ *
+ * @package Core
+ * @author Pango
+ *
+ * @since 1.0.00
+ */
+class FundaWande_Language {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+
+    }
+
+} // end FundaWande_Language
