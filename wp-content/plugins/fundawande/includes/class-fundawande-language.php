2e6995fbf492da355b41e86b27b461832987909b
Chris <chris@pango.world>
2018-08-01 12:33:48 +0200

    Add code functionality to the FW language get_language() function

--- wp-content/plugins/fundawande/includes/class-fundawande-language.php
+++ wp-content/plugins/fundawande/includes/class-fundawande-language.php
@@ -33,18 +33,38 @@ class FundaWande_Language {
     public function get_language( $language = null ) {
 
         // Determine if $language was assigned, if not then assign to the default language
+        if ($language === null ) {
+            // Assign default language, currently Xhosa
+            $language = 'xho';
+        }
 
         // Create empty $language_obj
+        $language_obj = new stdClass();
 
         // Create switch case depending on the $language
-
         // Add language prefix to $language_obj
-
         // Add language url suffix to $language_obj
-
         // Add language identifier to $language_obj
+        switch ($language) {
+            case 'xho':
+                $language_obj->prefix = 'xho-' ;
+                $language_obj->url_param = 'lang=xho';
+                $language_obj->id = 'xho';
+                break;
+            case 'eng':
+                $language_obj->prefix = 'eng-' ;
+                $language_obj->url_param = 'lang=eng';
+                $language_obj->id = 'eng';
+                break;
+            // Add default just as a catch all
+            default:
+                $language_obj->prefix = 'xho-' ;
+                $language_obj->url_param = 'lang=xho';
+                $language_obj->id = 'xho';
+        }
 
         // return $language_obj
+        return $language_obj;
 
 
     } // end get_language()
