e0be8f4b6e5c4c3645273167b411432ff033145c
Chris <chris@pango.world>
2018-08-02 16:53:45 +0200

    Add templates for lms - courses, quizzes, lessons

---
+++ wp-content/themes/fundawande/sensei/single-course.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The Template for displaying all single courses.
+ *
+ * Override this template by copying it to yourtheme/sensei/single-course.php
+ *
+ * @author 		Automattic
+ * @package 	Sensei
+ * @category    Templates
+ * @version     1.9.0
+ */
+
+// if user is not logged in direct to login page
+if (!is_user_logged_in()) {
+    wp_redirect(wp_login_url(get_permalink()));
+    exit();
+}
+
+if (class_exists('Timber')) {
+
+    $context = Timber::get_context();
+    $post = new TimberPost();
+
+    $context['post'] = $post;
+
+    // Assign current user to context
+    $user = new TimberUser();
+    $context['user'] = $user;
+
+
+    Timber::render(array('lms/single-course.twig', 'page.twig'), $context);
+}
