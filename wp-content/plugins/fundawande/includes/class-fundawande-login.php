92bb9df8fbb3f117d3aa2f815b376583e1e1837c
Jason Tame <jason@pango.studio>
2018-08-14 14:04:12 +0200

    Add login failed redirect logic

--- wp-content/plugins/fundawande/includes/class-fundawande-login.php
+++ wp-content/plugins/fundawande/includes/class-fundawande-login.php
@@ -18,7 +18,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class FundaWande_Login {
 
+    /**
+     * Constructor
+     */
+    public function __construct() {
 
+        //Add action to output custom error logic if the login fails
+        add_action('wp_login_failed', array($this, 'custom_login_failed'));
+
+    }
     //Set up login form options
     public function setup_login_form() {
         $args = array(
@@ -40,5 +48,37 @@ class FundaWande_Login {
         return wp_login_form( $args );
     } // end setup_login_form();
 
+    /**
+     * Refreshes the page and adds 'login=failed' to the URL in order to trigger custom error messages
+     *
+     * @param $user
+     *
+     * @author jtame
+     */
+    public function custom_login_failed ($user) {
+        $referrer = $_SERVER['HTTP_REFERER'];
+        if (!empty($referrer) && !strstr($referrer, 'wp-login') && !strstr($referrer, 'wp-admin') && $user!=null )
+        {
+            if (!strstr($referrer, '?login=failed'))
+            {
+                if(!strstr($referrer, '?'))
+                {
+                    $referrer .= '?';
+                }
+                else
+                {
+                    $referrer .= '&';
+                }
+
+                wp_redirect($referrer . 'login=failed');
+            }
+            else
+            {
+                wp_redirect($referrer);
+            }
+
+            exit;
+        }
+    }
 
 } // end FundaWande_Login
