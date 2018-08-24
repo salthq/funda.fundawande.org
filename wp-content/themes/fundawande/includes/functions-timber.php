d7c726dfbb7f7f70ea66503c136cdce6a284bd87
Jason Tame <jason@pango.studio>
2018-08-24 11:32:29 +0200

    Change role to user_role to make it more clear

--- wp-content/themes/fundawande/includes/functions-timber.php
+++ wp-content/themes/fundawande/includes/functions-timber.php
@@ -30,9 +30,6 @@ class StarterSite extends TimberSite {
 
 
     function add_to_context( $context ) {
-        $context['foo'] = 'bar';
-        $context['stuff'] = 'I am a value set in your functions.php file';
-        $context['notes'] = 'These values are available everytime you call Timber::get_context();';
         $context['learner_menu'] = new Timber\Menu('learner-menu');
         $context['coach_menu'] = new Timber\Menu('coach-menu');
         $context['site'] = $this;
