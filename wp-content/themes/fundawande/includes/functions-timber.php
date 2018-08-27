8646b5bb07845efd4318a66dc62fbe878fb2589c
Jason Tame <jason@pango.studio>
2018-08-27 15:32:43 +0200

    Add logout button link

--- wp-content/themes/fundawande/includes/functions-timber.php
+++ wp-content/themes/fundawande/includes/functions-timber.php
@@ -31,6 +31,7 @@ class StarterSite extends TimberSite {
 
     function add_to_context( $context ) {
         $context['user_logged_in'] = is_user_logged_in();
+        $context['log_out_link'] = wp_logout_url();
         $context['foo'] = 'bar';
         $context['stuff'] = 'I am a value set in your functions.php file';
         $context['notes'] = 'These values are available everytime you call Timber::get_context();';
