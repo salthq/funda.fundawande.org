f55a3db58c37ed2808e9acb9853fd784e6629534
Jason Tame <jason@pango.studio>
2018-09-12 15:14:11 +0200

    Create Lesson class in FW plugin

---
+++ wp-content/plugins/fundawande/includes/class-fundawande-lessons.php
@@ -0,0 +1,31 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // security check, don't load file outside WP
+}
+
+/**
+ * FundaWande Lessons Class
+ *
+ * All functionality pertaining to the custom lesson functionality of FundaWande.
+ *
+ * @package Core
+ * @author Pango
+ *
+ * @since 1.0.00
+ */
+
+ class FundaWande_Lessons {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+
+        // Add the construct elements
+
+    }
+
+    
+
+ } // end FundaWande_Lessons
