81d3a99bdb431c5e01d6284ff2bc3289ef36cdef
Chris <chris@pango.world>
2018-09-18 11:20:30 +0200

    Add FW version to the assets in functions.php

--- wp-content/themes/fundawande/functions.php
+++ wp-content/themes/fundawande/functions.php
@@ -19,12 +19,12 @@ const FW_VER = 0.03;
 function pango_scripts() {
 
     // Include all vender assets
-	wp_enqueue_style( 'vendors-style',  get_template_directory_uri().'/css/vendors-styles.min.css' ,array(),'1.1' );
-	wp_enqueue_script( 'vendors-script', get_template_directory_uri().'/js/vendors-js.min.js' ,array('jquery'),'1.1',true);
+	wp_enqueue_style( 'vendors-style',  get_template_directory_uri().'/css/vendors-styles.min.css' ,array(),FW_VER );
+	wp_enqueue_script( 'vendors-script', get_template_directory_uri().'/js/vendors-js.min.js' ,array('jquery'),FW_VER,true);
 
 	// Include global assets
-    wp_enqueue_style( 'theme-style',  get_template_directory_uri().'/css/theme-styles.min.css' ,array(),'1.1' );
-    wp_enqueue_script( 'theme-script', get_template_directory_uri().'/js/theme-js.min.js' ,array('jquery'),'1.2',true);
+    wp_enqueue_style( 'theme-style',  get_template_directory_uri().'/css/theme-styles.min.css' ,array(),FW_VER );
+    wp_enqueue_script( 'theme-script', get_template_directory_uri().'/js/theme-js.min.js' ,array('jquery'),FW_VER,true);
 
 }
 add_action( 'wp_enqueue_scripts', 'pango_scripts' );
@@ -33,7 +33,7 @@ add_action( 'wp_enqueue_scripts', 'pango_scripts' );
  * Enqueue admin scripts and styles.
  */
 function pango_admin_scripts() {
-    wp_enqueue_style( 'theme-admin-style',  get_template_directory_uri().'/css/admin-styles.min.css' ,array(),'1' );
+    wp_enqueue_style( 'theme-admin-style',  get_template_directory_uri().'/css/admin-styles.min.css' ,array(),FW_VER );
 
 }
 add_action( 'admin_enqueue_scripts', 'pango_admin_scripts' );
