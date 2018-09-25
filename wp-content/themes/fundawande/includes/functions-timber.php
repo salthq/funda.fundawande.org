b106ec00eec716fadc66a6d092629af2550686ca
Jason Tame <jason@pango.studio>
2018-09-25 12:13:57 +0200

    Make navbar bilingual, add current course link

--- wp-content/themes/fundawande/includes/functions-timber.php
+++ wp-content/themes/fundawande/includes/functions-timber.php
@@ -114,12 +114,10 @@ class FundaWandeSite extends TimberSite {
             $context['lang'] = FundaWande()->language->get_language(null);
         }
 
-        
-
         /**
          * Menu Context
         */
-
+        $context['course_link'] = get_permalink($context['user']->fw_current_course);
         $context['menu'] = new TimberMenu();
         $context['learner_menu'] = new Timber\Menu('learner-menu');
         $context['coach_menu'] = new Timber\Menu('coach-menu');
