1de606bff1588d17131824cef6489e4c4140a799
Chris <chris@pango.world>
2018-09-26 09:59:22 +0200

    Add logo

--- wp-content/themes/fundawande/taxonomy-module.php
+++ wp-content/themes/fundawande/taxonomy-module.php
@@ -22,6 +22,7 @@ if (class_exists('Timber')) {
 
     //Get module number to enable module-specific styling
     $context['module_number'] = get_term_meta($term->ID, 'module_number', true);
+    $context['module_title'] = get_term_meta($term->ID, 'module_title', true);
 
     // Get the modules units to visualise on the module page
     // TODO: Remove the 31 and replace with the actual course ID, possibly saved in the user as their current course.
