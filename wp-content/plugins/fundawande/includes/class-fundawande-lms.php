6db6d548ef407acb884e8478c3ce95bef273b8b8
Chris <chris@pango.world>
2018-09-26 10:37:14 +0200

    Make Sub Unit 0 the first

--- wp-content/plugins/fundawande/includes/class-fundawande-lms.php
+++ wp-content/plugins/fundawande/includes/class-fundawande-lms.php
@@ -700,7 +700,7 @@ class FundaWande_Lms {
         $sub_unit_key = '';
         if ($course_lessons[0] ) {
             $sub_unit_key = get_post_meta($course_lessons[0]->ID, 'fw_unique_key',true);
-            update_user_meta($user_id, 'fw_current_sub_unit',$sub_unit_key);
+            update_user_meta($user_id, 'fw_current_sub_unit','r4m_m00_u01_s01');
             update_user_meta($user_id,'fw_current_unit','r4m_m00_u01');
             update_user_meta($user_id,'fw_current_module','r4m_m00');
 
