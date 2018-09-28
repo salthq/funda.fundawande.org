adf2fdaf5295fe616dee8c35b78c91185281bb75
Jason Tame <jason@pango.studio>
2018-09-28 14:32:24 +0200

    Tweak to scroll to current script to stop console errors

--- wp-content/plugins/fundawande/assets/js/scroll-to-current.js
+++ wp-content/plugins/fundawande/assets/js/scroll-to-current.js
@@ -2,7 +2,7 @@ jQuery(document).ready( function($) {
 
     //If an element with the ID of 'current' exists on the page,
     //scroll to that element on page load.
-    if($('#current')) {
+    if($('#current').length) {
         $('html, body').animate({
             scrollTop: $('#current').offset().top - 100
         }, 500, 'linear');
