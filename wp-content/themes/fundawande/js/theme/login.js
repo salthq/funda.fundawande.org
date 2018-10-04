59aa49c745c15bc59f6f81346dbf44661a8f4d1a
Chris <chris@pango.world>
2018-10-04 12:13:18 +0200

    try fix captcha

--- wp-content/themes/fundawande/js/theme/login.js
+++ wp-content/themes/fundawande/js/theme/login.js
@@ -2,4 +2,13 @@ jQuery(document).ready( function($) {
     Intercom('update', {
         "hide_default_launcher": false
     });
+
+    var form = document.getElementById('fw-registration-form');
+    form.addEventListener("submit", function(event){
+            if (grecaptcha.getResponse() === '') {
+                event.preventDefault();
+                alert('Please check the recaptcha');
+            }
+        }
+        , false);
 });
