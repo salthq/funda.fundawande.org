44650a8555f33c74d4d41ea7a04a7f8589acc708
Jason Tame <jason@pango.studio>
2019-01-08 15:40:27 +0200

    Add check to make sure walkthroughs don't load on mobile devices

--- wp-content/plugins/fundawande/includes/class-fundawande-frontend.php
+++ wp-content/plugins/fundawande/includes/class-fundawande-frontend.php
@@ -115,7 +115,7 @@ class FundaWande_Frontend {
 			// TODO: Remove the initial tour script below
 			//If this is the first time that the user has landed on the lesson page, 
 			//and the terms and conditions have been signed, the script will load.
-			if(get_user_meta($user_id, 'first-visit', true) == 0 && get_user_meta($user_id, 'legal', true) == 'agreed') {
+			if(get_user_meta($user_id, 'first-visit', true) == 0 && get_user_meta($user_id, 'legal', true) == 'agreed' && !wp_is_mobile()) {
 				wp_enqueue_script('initial-tour-script', FundaWande()->plugin_url . 'assets/js/tour-scripts/'.$language.'-initial-tour.min.js', array('jquery'), FundaWande()->version, true);
 				update_user_meta($user_id, 'first-visit', 1);
 			}
