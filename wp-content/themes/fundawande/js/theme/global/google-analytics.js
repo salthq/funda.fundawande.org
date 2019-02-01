e817afb22bc70fdb8a8f09b99419988546902adc
James Lees <jamestrevorlees@gmail.com>
2019-02-01 12:28:07 +0200

    add js file to house ga code and then add the tracking ID script from Google Analytics

---
+++ wp-content/themes/fundawande/js/theme/global/google-analytics.js
@@ -0,0 +1,11 @@
+/* Add the Funda Wande Web App Google Analytics Master tracking code - DO NOT REMOVER EVER */
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123486385-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-123486385-3');
+</script>
