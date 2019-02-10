e02b28ebd0fdb35fe5773b5821ace4e1539547f9
Jason Tame <jason@pango.studio>
2019-02-10 19:26:32 +0200

    Add focus to toggle button when video has been clicked

--- wp-content/themes/fundawande/js/theme/global/video-player.js
+++ wp-content/themes/fundawande/js/theme/global/video-player.js
@@ -99,12 +99,16 @@ players.forEach(function (player) {
     // Handle mouse/finger scrubbing
     function scrub(e) {
         const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-        video.currentTime = scrubTime;
         const percent = (scrubTime / video.duration) * 100;
         progressBar.style.flexBasis = percent + "%";
         progressBar.style.width = percent + "%";
     }
 
+    // Handle mouse/finger scrubbing
+    function scrubEnd(e) {
+        const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+        video.currentTime = scrubTime;
+    }
 
     // Handle muting
     function muteToggle() {
@@ -149,8 +153,8 @@ players.forEach(function (player) {
     function playerStart() {
         start.style.display = "none";
         controls.style.display = "flex";
+        toggle.focus(); 
         togglePlay();
-
 
     }
 
