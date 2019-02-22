1d1b90546915ada7be8336baaafb60c608fae581
Jason Tame <jason@pango.studio>
2019-02-22 12:42:04 +0200

    Add initial coach assessment dashboard tour scripts

--- wp-content/plugins/fundawande/assets/js/tour-scripts/eng-coach-review-tour.js
+++ wp-content/plugins/fundawande/assets/js/tour-scripts/eng-coach-review-tour.js
@@ -1,9 +1,83 @@
 // Coach Dashboard Tour
 var tour = new Anno([{
-    target: '#coach-dash-wrapper',
-    position: {
-        top: '20em'
-    },
+    target: '#view-tooltips',
+    position: 'bottom',
     content: "Welcome to the coach assessment review dashboard. From here you can manage all assessments which need feedback.",
-    buttons: [AnnoButton.DoneButton]
-  }])
+    buttons: [
+      {
+        text: "Next",
+        className: "pulse",
+        click: function() {
+          return this.switchToChainNext();
+        }
+      }
+    ],  },
+  {
+    target: '#progress-dash-btn',
+    position: 'bottom',
+    content: "Click here to view learner progress information",
+    buttons: [
+      {
+        text: "Back",
+        className: "anno-btn-low-importance",
+        click: function() {
+          return this.switchToChainPrev();
+        }
+      },
+      {
+        text: "Next",
+        className: "pulse",
+        click: function() {
+          return this.switchToChainNext();
+        }
+      }
+    ],
+  },
+  {
+    target: '#coach-dash-filters',
+    position: 'bottom',
+    content: "Select filters to view the course information",
+    buttons: [
+      {
+        text: "Back",
+        className: "anno-btn-low-importance",
+        click: function() {
+          return this.switchToChainPrev();
+        }
+      },
+      {
+        text: "Next",
+        className: "pulse",
+        click: function() {
+          return this.switchToChainNext();
+        }
+      }
+    ],
+  },
+  {
+    target: '#courseSelect',
+    position: 'right',
+    content: "To get started, select a course",
+    buttons: [
+      {
+        text: "Back",
+        className: "anno-btn-low-importance",
+        click: function() {
+          return this.switchToChainPrev();
+        }
+      },
+      {
+        text: "Next",
+        className: "pulse",
+        click: function() {
+          return this.switchToChainNext();
+        }
+      }
+    ],
+  },
+  {
+    target: '#coach-dash-filters-submit',
+    position: 'right',
+    content: "And click here to view the data table"
+  }
+])
