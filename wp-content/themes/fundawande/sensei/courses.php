cccecd8a8c15e1cca86f630a219b3de58fbb29c5
Jason Tame <jason@pango.studio>
2019-03-01 14:59:54 +0200

    Align course card styling to wireframes

--- wp-content/themes/fundawande/sensei/courses.php
+++ wp-content/themes/fundawande/sensei/courses.php
@@ -22,6 +22,7 @@ if (class_exists('Timber')) {
     // Assign current user to context
     $user = new TimberUser();
     $context['user'] = $user;
+    $context['current_course'] = $user->fw_current_course;
 
     $course_args = array(
         'post_type' => 'course',
@@ -38,6 +39,11 @@ if (class_exists('Timber')) {
         ) {
             $course->completed_lessons = count(Sensei()->course->get_completed_lesson_ids($course->ID, get_current_user_id()));
             $course->lesson_count = count(Sensei()->course->course_lessons($course->ID));
+            if ($course->lesson_count > 0) {
+                $course->completion_percentage = round($course->completed_lessons / $course->lesson_count * 100);
+            } else {
+                $course->completion_percentage = 0;
+            }
         }
     }
 
