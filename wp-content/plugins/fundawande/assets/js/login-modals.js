c23e5df72c2d784b348add8b5b5e501cb8cb554c
Jason Tame <jason@pango.studio>
2019-03-26 16:03:54 +0200

    Re-add language picker modal on first login, add various missing strings

--- wp-content/plugins/fundawande/assets/js/login-modals.js
+++ wp-content/plugins/fundawande/assets/js/login-modals.js
@@ -1,19 +1,17 @@
-jQuery(document).ready( function($) {
+jQuery(document).ready(function($) {
+  // TODO: Delete line 5 and uncomment line 4 once language switching is re-enabled
+  $("#language-preference").modal("show");
+  // $('#terms-and-conditions').modal('show');
 
-    // TODO: Delete line 5 and uncomment line 4 once language switching is re-enabled
-    // $('#set-language-preference').modal('show');
-    $('#terms-and-conditions').modal('show');
+  // //Once the continue button is clicked on the language preference modal, the Ts & Cs modal fires.
+  // TODO: Uncomment the lines below once language switching is re-enabled
+  $("#set-language-preference").click(function() {
+    $("#language-preference").modal("hide");
+    $("#terms-and-conditions").modal("show");
+  });
 
-    // //Once the continue button is clicked on the language preference modal, the Ts & Cs modal fires.
-    // TODO: Uncomment the lines below once language switching is re-enabled
-    // $('#set-language-preference').click(function() {
-    //     $('#language-preference').modal('hide');
-    //     $('#terms-and-conditions').modal('show');
-    // });
-
-    //Trigger a reload when the terms and conditions are signed, so the user is directed to their course
-    $('#go-to-course').click(function() {
-        location.reload('true');
-    });
-
+  //Trigger a reload when the terms and conditions are signed, so the user is directed to their course
+  $("#go-to-course").click(function() {
+    location.reload("true");
+  });
 });
