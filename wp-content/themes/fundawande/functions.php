cd5507817672edd0d0e4261911be81b424592872
Jason Tame <jason@pango.studio>
2019-06-19 09:22:25 +0200

    Change version to 1.2.3

--- wp-content/themes/fundawande/functions.php
+++ wp-content/themes/fundawande/functions.php
@@ -10,7 +10,7 @@
 
 // Set up constant variable to control JS & CSS versioning
 // This version number must be changed whenever pushing to stable branch
-const FW_VER = '1.2.2'; // Released on 13/06/2019 by Jason Tame
+const FW_VER = '1.2.3'; // Released on 19/06/2019 by Jason Tame
 
 
 /**
